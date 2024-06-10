29e771db3713c03509808e06281f19147618882f
mafumaful <s6bian@uwaterloo.ca>
2024-06-09 22:18:22 -0400

    Optimize A* algorithm implementation in Astar_searcher.cpp

--- hw_2/src/grid_path_searcher/src/demo_node.cpp
+++ hw_2/src/grid_path_searcher/src/demo_node.cpp
@@ -21,16 +21,17 @@
 using namespace std;
 using namespace Eigen;
 
-namespace backward {
-backward::SignalHandling sh;
+namespace backward
+{
+    backward::SignalHandling sh;
 }
 
 // simulation param from launch file
 double _resolution, _inv_resolution, _cloud_margin;
-double _x_size, _y_size, _z_size;    
+double _x_size, _y_size, _z_size;
 
 // useful global variables
-bool _has_map   = false;
+bool _has_map = false;
 
 Vector3d _start_pt;
 Vector3d _map_lower, _map_upper;
@@ -38,48 +39,50 @@ int _max_x_id, _max_y_id, _max_z_id;
 
 // ros related
 ros::Subscriber _map_sub, _pts_sub;
-ros::Publisher  _grid_path_vis_pub, _visited_nodes_vis_pub, _grid_map_vis_pub;
+ros::Publisher _grid_path_vis_pub, _visited_nodes_vis_pub, _grid_map_vis_pub;
 
-AstarPathFinder * _astar_path_finder     = new AstarPathFinder();
-JPSPathFinder   * _jps_path_finder       = new JPSPathFinder();
+AstarPathFinder *_astar_path_finder = new AstarPathFinder();
+JPSPathFinder *_jps_path_finder = new JPSPathFinder();
 
-void rcvWaypointsCallback(const nav_msgs::Path & wp);
-void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 & pointcloud_map);
+void rcvWaypointsCallback(const nav_msgs::Path &wp);
+void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 &pointcloud_map);
 
-void visGridPath( vector<Vector3d> nodes, bool is_use_jps );
-void visVisitedNode( vector<Vector3d> nodes );
+void visGridPath(vector<Vector3d> nodes, bool is_use_jps);
+void visVisitedNode(vector<Vector3d> nodes);
 void pathFinding(const Vector3d start_pt, const Vector3d target_pt);
 
-void rcvWaypointsCallback(const nav_msgs::Path & wp)
-{     
-    if( wp.poses[0].pose.position.z < 0.0 || _has_map == false )
+void rcvWaypointsCallback(const nav_msgs::Path &wp)
+{
+    if (wp.poses[0].pose.position.z < 0.0 || _has_map == false)
         return;
 
     Vector3d target_pt;
     target_pt << wp.poses[0].pose.position.x,
-                 wp.poses[0].pose.position.y,
-                 wp.poses[0].pose.position.z;
+        wp.poses[0].pose.position.y,
+        wp.poses[0].pose.position.z;
 
     ROS_INFO("[node] receive the planning target");
-    pathFinding(_start_pt, target_pt); 
+    pathFinding(_start_pt, target_pt);
 }
 
-void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 & pointcloud_map)
-{   
-    if(_has_map ) return;
+void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 &pointcloud_map)
+{
+    if (_has_map)
+        return;
 
     pcl::PointCloud<pcl::PointXYZ> cloud;
     pcl::PointCloud<pcl::PointXYZ> cloud_vis;
     sensor_msgs::PointCloud2 map_vis;
 
     pcl::fromROSMsg(pointcloud_map, cloud);
-    
-    if( (int)cloud.points.size() == 0 ) return;
+
+    if ((int)cloud.points.size() == 0)
+        return;
 
     pcl::PointXYZ pt;
     for (int idx = 0; idx < (int)cloud.points.size(); idx++)
-    {    
-        pt = cloud.points[idx];        
+    {
+        pt = cloud.points[idx];
 
         // set obstalces into grid map for path planning
         _astar_path_finder->setObs(pt.x, pt.y, pt.z);
@@ -93,8 +96,8 @@ void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 & pointcloud_map)
         cloud_vis.points.push_back(pt);
     }
 
-    cloud_vis.width    = cloud_vis.points.size();
-    cloud_vis.height   = 1;
+    cloud_vis.width = cloud_vis.points.size();
+    cloud_vis.height = 1;
     cloud_vis.is_dense = true;
 
     pcl::toROSMsg(cloud_vis, map_vis);
@@ -107,86 +110,86 @@ void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 & pointcloud_map)
 
 void pathFinding(const Vector3d start_pt, const Vector3d target_pt)
 {
-    //Call A* to search for a path
+    // Call A* to search for a path
     _astar_path_finder->AstarGraphSearch(start_pt, target_pt);
 
-    //Retrieve the path
-    auto grid_path     = _astar_path_finder->getPath();
+    // Retrieve the path
+    auto grid_path = _astar_path_finder->getPath();
     auto visited_nodes = _astar_path_finder->getVisitedNodes();
 
-    //Visualize the result
-    visGridPath (grid_path, false);
+    // Visualize the result
+    visGridPath(grid_path, false);
     visVisitedNode(visited_nodes);
 
-    //Reset map for next call
+    // Reset map for next call
     _astar_path_finder->resetUsedGrids();
 
     //_use_jps = 0 -> Do not use JPS
     //_use_jps = 1 -> Use JPS
-    //you just need to change the #define value of _use_jps
+    // you just need to change the #define value of _use_jps
 #define _use_jps 0
 #if _use_jps
     {
-        //Call JPS to search for a path
-        _jps_path_finder -> JPSGraphSearch(start_pt, target_pt);
+        // Call JPS to search for a path
+        _jps_path_finder->JPSGraphSearch(start_pt, target_pt);
 
-        //Retrieve the path
-        auto grid_path     = _jps_path_finder->getPath();
+        // Retrieve the path
+        auto grid_path = _jps_path_finder->getPath();
         auto visited_nodes = _jps_path_finder->getVisitedNodes();
 
-        //Visualize the result
-        visGridPath   (grid_path, _use_jps);
+        // Visualize the result
+        visGridPath(grid_path, _use_jps);
         visVisitedNode(visited_nodes);
 
-        //Reset map for next call
+        // Reset map for next call
         _jps_path_finder->resetUsedGrids();
     }
 #endif
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "demo_node");
     ros::NodeHandle nh("~");
 
-    _map_sub  = nh.subscribe( "map",       1, rcvPointCloudCallBack );
-    _pts_sub  = nh.subscribe( "waypoints", 1, rcvWaypointsCallback );
+    _map_sub = nh.subscribe("map", 1, rcvPointCloudCallBack);
+    _pts_sub = nh.subscribe("waypoints", 1, rcvWaypointsCallback);
 
-    _grid_map_vis_pub             = nh.advertise<sensor_msgs::PointCloud2>("grid_map_vis", 1);
-    _grid_path_vis_pub            = nh.advertise<visualization_msgs::Marker>("grid_path_vis", 1);
-    _visited_nodes_vis_pub        = nh.advertise<visualization_msgs::Marker>("visited_nodes_vis",1);
+    _grid_map_vis_pub = nh.advertise<sensor_msgs::PointCloud2>("grid_map_vis", 1);
+    _grid_path_vis_pub = nh.advertise<visualization_msgs::Marker>("grid_path_vis", 1);
+    _visited_nodes_vis_pub = nh.advertise<visualization_msgs::Marker>("visited_nodes_vis", 1);
 
-    nh.param("map/cloud_margin",  _cloud_margin, 0.0);
-    nh.param("map/resolution",    _resolution,   0.2);
-    
-    nh.param("map/x_size",        _x_size, 50.0);
-    nh.param("map/y_size",        _y_size, 50.0);
-    nh.param("map/z_size",        _z_size, 5.0 );
-    
-    nh.param("planning/start_x",  _start_pt(0),  0.0);
-    nh.param("planning/start_y",  _start_pt(1),  0.0);
-    nh.param("planning/start_z",  _start_pt(2),  0.0);
+    nh.param("map/cloud_margin", _cloud_margin, 0.0);
+    nh.param("map/resolution", _resolution, 0.2);
 
-    _map_lower << - _x_size/2.0, - _y_size/2.0,     0.0;
-    _map_upper << + _x_size/2.0, + _y_size/2.0, _z_size;
-    
+    nh.param("map/x_size", _x_size, 50.0);
+    nh.param("map/y_size", _y_size, 50.0);
+    nh.param("map/z_size", _z_size, 5.0);
+
+    nh.param("planning/start_x", _start_pt(0), 0.0);
+    nh.param("planning/start_y", _start_pt(1), 0.0);
+    nh.param("planning/start_z", _start_pt(2), 0.0);
+
+    _map_lower << -_x_size / 2.0, -_y_size / 2.0, 0.0;
+    _map_upper << +_x_size / 2.0, +_y_size / 2.0, _z_size;
+
     _inv_resolution = 1.0 / _resolution;
-    
+
     _max_x_id = (int)(_x_size * _inv_resolution);
     _max_y_id = (int)(_y_size * _inv_resolution);
     _max_z_id = (int)(_z_size * _inv_resolution);
 
-    _astar_path_finder  = new AstarPathFinder();
-    _astar_path_finder  -> initGridMap(_resolution, _map_lower, _map_upper, _max_x_id, _max_y_id, _max_z_id);
+    _astar_path_finder = new AstarPathFinder();
+    _astar_path_finder->initGridMap(_resolution, _map_lower, _map_upper, _max_x_id, _max_y_id, _max_z_id);
 
-    _jps_path_finder    = new JPSPathFinder();
-    _jps_path_finder    -> initGridMap(_resolution, _map_lower, _map_upper, _max_x_id, _max_y_id, _max_z_id);
-    
+    _jps_path_finder = new JPSPathFinder();
+    _jps_path_finder->initGridMap(_resolution, _map_lower, _map_upper, _max_x_id, _max_y_id, _max_z_id);
+
     ros::Rate rate(100);
     bool status = ros::ok();
-    while(status) 
+    while (status)
     {
-        ros::spinOnce();      
+        ros::spinOnce();
         status = ros::ok();
         rate.sleep();
     }
@@ -196,13 +199,13 @@ int main(int argc, char** argv)
     return 0;
 }
 
-void visGridPath( vector<Vector3d> nodes, bool is_use_jps )
-{   
-    visualization_msgs::Marker node_vis; 
+void visGridPath(vector<Vector3d> nodes, bool is_use_jps)
+{
+    visualization_msgs::Marker node_vis;
     node_vis.header.frame_id = "world";
     node_vis.header.stamp = ros::Time::now();
-    
-    if(is_use_jps)
+
+    if (is_use_jps)
         node_vis.ns = "demo_node/jps_path";
     else
         node_vis.ns = "demo_node/astar_path";
@@ -216,26 +219,27 @@ void visGridPath( vector<Vector3d> nodes, bool is_use_jps )
     node_vis.pose.orientation.z = 0.0;
     node_vis.pose.orientation.w = 1.0;
 
-    if(is_use_jps){
+    if (is_use_jps)
+    {
         node_vis.color.a = 1.0;
         node_vis.color.r = 1.0;
         node_vis.color.g = 0.0;
         node_vis.color.b = 0.0;
     }
-    else{
+    else
+    {
         node_vis.color.a = 1.0;
         node_vis.color.r = 0.0;
         node_vis.color.g = 1.0;
         node_vis.color.b = 0.0;
     }
 
-
     node_vis.scale.x = _resolution;
     node_vis.scale.y = _resolution;
     node_vis.scale.z = _resolution;
 
     geometry_msgs::Point pt;
-    for(int i = 0; i < int(nodes.size()); i++)
+    for (int i = 0; i < int(nodes.size()); i++)
     {
         Vector3d coord = nodes[i];
         pt.x = coord(0);
@@ -248,9 +252,9 @@ void visGridPath( vector<Vector3d> nodes, bool is_use_jps )
     _grid_path_vis_pub.publish(node_vis);
 }
 
-void visVisitedNode( vector<Vector3d> nodes )
-{   
-    visualization_msgs::Marker node_vis; 
+void visVisitedNode(vector<Vector3d> nodes)
+{
+    visualization_msgs::Marker node_vis;
     node_vis.header.frame_id = "world";
     node_vis.header.stamp = ros::Time::now();
     node_vis.ns = "demo_node/expanded_nodes";
@@ -272,7 +276,7 @@ void visVisitedNode( vector<Vector3d> nodes )
     node_vis.scale.z = _resolution;
 
     geometry_msgs::Point pt;
-    for(int i = 0; i < int(nodes.size()); i++)
+    for (int i = 0; i < int(nodes.size()); i++)
     {
         Vector3d coord = nodes[i];
         pt.x = coord(0);
